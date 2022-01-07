59ac3e66372a3f434d821993b9f795b2cee7b593
chidiebereojingwa <ojingwaoji@gmail.com>
2022-01-07 16:32:40 +0100

    Updated data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const sliderItems = [
       id: 1,
       img: "https://pbs.twimg.com/media/D0bXv64WsAAxjJb.jpg",
       title: "FRESH TOMATOTOES FROM THE FARM",
-      desc: "DON'T COMPROMISE ON QAULITY POCKET FRIENDLY! SAFE 30% FLAT BY BUYING DIRECT FROM FARMERS .",
+      desc: "DON'T COMPROMISE ON QAULITY/POCKET FRIENDLY! SAFE 30% FLAT BY BUYING DIRECT FROM FARMERS .",
       bg: "f5fafd",
     },
     {
@@ -37,6 +37,21 @@ export const sliderItems = [
       id: 3,
       img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       title: "LIGHT JACKETS",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+    {
+      id: 5,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 6,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
     },
   ];
 
