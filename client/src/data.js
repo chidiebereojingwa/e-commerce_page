bf402f42816afa29cb29e8522f70e213e3439703
chidiebereojingwa <ojingwaoji@gmail.com>
2022-01-22 12:20:31 +0100

    Updated slider

--- client/src/data.js
+++ client/src/data.js
@@ -8,7 +8,7 @@ export const sliderItems = [
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
+      img: "https://entrackr.com/wp-content/uploads/2020/03/zomato-grocery-.jpg.webp",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
